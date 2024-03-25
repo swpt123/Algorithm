1b3440df1ec26d844ebc81500a396e9226bf0089
20243104 <A20243104@HackerLogin.8.8.8.8>
2024-03-25 20:43:14 +0900

    2557 solve

--- baekjoon/2557/2557.c
+++ baekjoon/2557/2557.c
@@ -1,10 +1,7 @@
+
 #include <stdio.h>
 
 int main(void){
-    int a = 1;
-    unsigned int k = 0;
-    float b = 2.4;
-    char c = 'A';
-    printf("%d, %f, %c\n", a, b, c);
-    printf("%f, %d, %d\n", a, b, c);
+    printf("Hello World!\n");
+    return 0;
 }
