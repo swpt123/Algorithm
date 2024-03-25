d4fedf2b5dd7eeb4150d1ed8f926b80358add689
swpt123 <A20243104@HackerLogin.8.8.8.8>
2024-03-25 20:34:09 +0900

    solve 2557

---
+++ baekjoon/2557/2557.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void){
+    int a = 1;
+    unsigned int k = 0;
+    float b = 2.4;
+    char c = 'A';
+    printf("%d, %f, %c\n", a, b, c);
+    printf("%f, %d, %d\n", a, b, c);
+}
